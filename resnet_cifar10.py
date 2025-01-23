4c63d029746f8cbf11082506d8149d1cf3b09dd1
Lauren Sun <laurenlsun@gmail.com>
2025-01-23 02:12:22 +0000

    define wideresnet class

--- resnet_cifar10.py
+++ resnet_cifar10.py
@@ -602,6 +602,42 @@ class ResNetLR(nn.Module):
         out = self.linear(out)
         return out
 
+# ----------- new Wide Resnet -------------#
+
+class WideResNet(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10, k=2): # k=factor by which to widen
+        super(ResNet, self).__init__()
+        self.in_planes = 64*k
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        #self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64*k, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128*k, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256*k, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512*k, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion*k, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        #out = F.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.conv1(x))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+
+
 
 def ResNet18():
     return ResNet(BasicBlock, [2,2,2,2])
@@ -664,3 +700,8 @@ if __name__ == "__main__":
 
     #for name, p in model.named_parameters():
     #    print(name, p.requires_grad)
+
+
+def WideResNet18():
+    return WideResNet(BasicBlock, [2,2,2,2], 2)
+
